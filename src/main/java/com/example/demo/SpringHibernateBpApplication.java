da5c36705cf02b29257a42bb111f773037bee8e8
sudhanshu singh chauhan <sudhanshu@heady.io>
2019-07-10 20:21:14 +0530

    add spring boot starter

---
+++ src/main/java/com/example/demo/SpringHibernateBpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateBpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateBpApplication.class, args);
+	}
+
+}
